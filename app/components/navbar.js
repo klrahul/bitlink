8649ce61a646c05c68c2874a445a66d4a78922c2
klrahul <your-email@example.com>
2025-02-04 15:42:04 +0530

    initial

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -5,16 +5,21 @@ const Navbar = () => {
   return (
     <div>
       <nav className='bg-purple-700 flex items-center justify-between py-3 px-10 absolute w-[100%]'>
-        <div className='font-bold text-2xl cursor-pointer'><Link href='/'>BitLinks</Link></div>
-         <ul className='flex gap-5 items-center'>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/shortner">Shortner</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li className='flex gap-5'>
-                <button className='bg-purple-500 rounded-md py-1 px-2'>Try Now</button>
-                <button className='bg-purple-500 rounded-md py-1 px-2'>Github</button>
-            </li>
-         </ul>
+        <div className='font-bold text-2xl cursor-pointer'><Link className="gap-2 flex items-center" href='/'>
+        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 13a5 5 0 0 1 0-7l3-3a5 5 0 0 1 7 7l-1 1M14 11a5 5 0 0 1 0 7l-3 3a5 5 0 0 1-7-7l1-1"/>
+</svg>
+
+BitLinks</Link></div>
+        <ul className='flex gap-5 items-center'>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/shortner">Shortner</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li className='flex gap-5'>
+            <Link className='bg-purple-500 rounded-md py-1 px-2' href="/shortner">Try Now</Link>
+            <Link href="https://github.com/klrahul/bitlink" className='bg-purple-500 rounded-md py-1 px-2'>Github</Link>
+          </li>
+        </ul>
       </nav>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image";
 import { Poppins } from 'next/font/google';
+import { useRef, useState, useEffect } from "react";
 
 // Import Poppins with specific weights for Content component
 const poppins = Poppins({
@@ -7,19 +9,43 @@ const poppins = Poppins({
   subsets: ['latin'],
 });
 export default function Home() {
+  const texts = ["Fastest URL shortener", "Privacy-focused"];
+    const [textIndex, setTextIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+    const [displayText, setDisplayText] = useState("");
+
+    useEffect(() => {
+        if (charIndex < texts[textIndex].length) {
+            const timeout = setTimeout(() => {
+                setDisplayText(prev => prev + texts[textIndex][charIndex]);
+                setCharIndex(charIndex + 1);
+            }, 100);
+            return () => clearTimeout(timeout);
+        } else {
+            const timeout = setTimeout(() => {
+                setDisplayText("");
+                setCharIndex(0);
+                setTextIndex((textIndex + 1) % texts.length);
+            }, 2000);
+            return () => clearTimeout(timeout);
+        }
+    }, [charIndex, textIndex]);
   return (
     <div className="bg-purple-50 min-h-screen flex">
        <section className="grid grid-cols-2 bg-purple-200 py-10">
-         <div className="bg-purple-200 flex justify-center items-center flex-col text-black">
-            <h1 className={`text-2xl font-extrabold ${poppins.className}`}>The best URL shortner in the market</h1>
-            <span className={`text-center text-sm px-32 py-3 text-md ${poppins.className}`}>
+         <div className="bg-purple-200 flex justify-center items-center flex-start flex-col text-black px-5">
+            <h1 className={`text-3xl font-extrabold ${poppins.className}`}>The Best URL shortner in the market</h1>
+            <span className={`text-center text-sm px-32 py-6 text-md ${poppins.className}`}>
               We are most Straightforward and easy to use URL shortner in the market.
               We provide you with the best analytics to track your links.
               and we are free to use. We are the best in the market.
             </span>
+            <div className="flex">
+                  <h1 className="text-xl font-bold my-5 text-gray-500"><span id="typing" className="cursor">{displayText}</span></h1>
+            </div>
             <div className="flex gap-5 text-white font-weight-bold">
-              <a href="/shortner" className='bg-purple-500 rounded-md py-1 px-3'>Try Now</a>
-               <button className='bg-purple-500 rounded-md py-1 px-3'>Github</button>
+              <a href="/shortner" className='bg-purple-500 rounded-md py-2 px-5 font-bold'>Try Now</a>
+               <a href="https://github.com/klrahul/bitlink" className='bg-transparent border-[1px] border-black  text-black rounded-md py-2 px-5 font-bold'>Github</a>
             </div>
          </div>
          <div className="flex justify-center items-center">
